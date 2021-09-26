45bd31f2fa98712a48d5693bfb910d254f01ac36
y4shiro <y4shironao@gmail.com>
2021-09-26 21:28:53 +0900

    従来どおり .css ファイルでスタイル定義も可能

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,29 +28,6 @@ const Home: NextPage = () => {
         </li>
       </ul>
       <h1 className="heading">Hello Next.js</h1>
-      <Content type="alert" />
-      <p>ここにもコンテンツが入ります。</p>
-      <style>
-        {`
-          .heading {
-            color: red;
-            background: green;
-          }
-        `}
-      </style>
-    </div>
-  );
-};
-
-const Content = ({ type }) => {
-  return (
-    <div>
-      <p>ここにコンテンツが入ります。</p>
-      <style global jsx>{`
-        p {
-          color: ${type == 'alert' ? 'red' : 'blue'};
-        }
-      `}</style>
     </div>
   );
 };
