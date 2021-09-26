49e8bf2f55d3bb2fed99cbf01d91c252f0c84c21
y4shiro <y4shironao@gmail.com>
2021-09-26 19:12:11 +0900

    個別の post ページにて、fetch で取得した Post データをページに表示する

--- pages/posts/[post].tsx
+++ pages/posts/[post].tsx
@@ -1,7 +1,20 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 
-const post: NextPage = () => {
-  return <h1>POST(投稿)</h1>;
+const post: NextPage = ({ post }) => {
+  return (
+    <div>
+      <h1>POST(投稿){post.id}</h1>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({ params }) => {
+  const id = params.post;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const post = await res.json();
+  return { props: { post } };
 };
 
 export default post;
