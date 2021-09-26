09aaf244d9e5dd0df4035a8363effdc90fa3c624
y4shiro <y4shironao@gmail.com>
2021-09-26 17:42:11 +0900

    配列を利用してリンクの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 
+const products = [{ name: 'bag' }, { name: 'shoes' }, { name: 'socks' }];
+
 const Home: NextPage = () => {
   return (
     <div>
       <ul>
+        {products.map((product) => (
+          <li key={product.name}>
+            <Link href={`/products/${product.name}`}>
+              <a>{product.name}</a>
+            </Link>
+          </li>
+        ))}
         <li>
           <Link href="/about">
             <a>About</a>
